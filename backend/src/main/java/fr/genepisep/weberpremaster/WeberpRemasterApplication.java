82a71486edc21d2e09512683a2ff11c7c2f78284
Maxime TARDIEU <mtardieu@juniorisep.com>
2024-02-07 11:42:37 +0100

    Init java

---
+++ backend/src/main/java/fr/genepisep/weberpremaster/WeberpRemasterApplication.java
@@ -0,0 +1,13 @@
+package fr.genepisep.weberpremaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeberpRemasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeberpRemasterApplication.class, args);
+	}
+
+}
