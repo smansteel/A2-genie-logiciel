2b2563d5f3f25aa488acedbbc009745d9e516bcc
Maxime TARDIEU <mtardieu@juniorisep.com>
2024-05-15 10:19:32 +0200

    cors

--- backend/src/main/java/fr/genepisep/icompetences/controllers/SecurityController.java
+++ backend/src/main/java/fr/genepisep/icompetences/controllers/SecurityController.java
@@ -6,6 +6,7 @@ import fr.genepisep.icompetences.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import javax.security.auth.login.FailedLoginException;
 
 @Controller
 @RequestMapping("auth")
+
 public class SecurityController {
 
     @Autowired
