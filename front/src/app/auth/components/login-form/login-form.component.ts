bb63f975ae9f43d0207738f1fdad4d9529a9ce6f
Matthieu <matthieu.delarue5@gmail.com>
2024-05-15 09:37:25 +0200

    ✨ Auth logic

--- front/src/app/auth/components/login-form/login-form.component.ts
+++ front/src/app/auth/components/login-form/login-form.component.ts
@@ -1,27 +1,32 @@
 import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: "app-login-form",
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: "./login-form.component.html",
   styleUrl: "./login-form.component.css",
 })
 export class LoginFormComponent {
-  email: string = "";
-  password: string = "";
+  loginForm = this.fb.group({
+    email: ["", [Validators.required, Validators.email, Validators.minLength(5)]],
+    password: ["", [Validators.required, Validators.minLength(8)]],
+  });
 
   constructor(
     private router: Router,
     private auth: AuthService,
+    private fb: FormBuilder,
   ) {}
 
   login() {
-    console.log("OK");
-    this.auth.login(this.email, this.password);
+    const formValues = this.loginForm.value;
+    if (this.loginForm.invalid) return;
+
+    this.auth.login(formValues.email!, formValues.password!);
   }
 
   register() {
--- front/src/app/auth/components/register-form/register-form.component.ts
+++ front/src/app/auth/components/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
 import { CommonModule } from "@angular/common";
