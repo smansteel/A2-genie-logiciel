4f96eb6ee608c542cd19708800cf29fe0c564b3f
Maxime TARDIEU <mtardieu@juniorisep.com>
2024-06-06 10:36:24 +0200

    test remote dev

--- backend/src/main/java/fr/genepisep/icompetences/configuration/SecurityConfig.java
+++ backend/src/main/java/fr/genepisep/icompetences/configuration/SecurityConfig.java
@@ -1,88 +1,93 @@
 package fr.genepisep.icompetences.configuration;
 
-import fr.genepisep.icompetences.security.JwtAuthFilter;
-import fr.genepisep.icompetences.security.MyCorsFilter;
-import jakarta.servlet.http.HttpServletRequest;
+import fr.genepisep.icompetences.services.IsepUserDetailsService;
+import org.apereo.cas.client.session.SingleSignOutFilter;
+import org.apereo.cas.client.validation.Cas30ServiceTicketValidator;
+import org.apereo.cas.client.validation.TicketValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.cas.ServiceProperties;
+import org.springframework.security.cas.authentication.CasAuthenticationProvider;
+import org.springframework.security.cas.web.CasAuthenticationEntryPoint;
+import org.springframework.security.cas.web.CasAuthenticationFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    @Order(1)
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
-        return http.cors(cors -> cors.configurationSource(apiConfigurationSource()))
-                .authorizeHttpRequests(httpRq -> httpRq.anyRequest().permitAll())
-                .sessionManagement((session) ->
-                        session
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+    public SecurityFilterChain filterChain(HttpSecurity http, IsepUserDetailsService isepUserDetailsService) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(casAuthenticationEntryPoint())
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilter(casAuthenticationFilter(isepUserDetailsService))
+                .addFilterBefore(singleSignOutFilter(), CasAuthenticationFilter.class)
+                .addFilterBefore(logoutFilter(), LogoutFilter.class);
 
+        return http.build();
     }
 
     @Bean
-    @Order(0)
-    public SecurityFilterChain filterChain2(HttpSecurity http, MyCorsFilter myCorsFilter) throws Exception {
-        return http.cors(cors -> cors.configurationSource(apiConfigurationSource()))
-                .authorizeHttpRequests(httpRq -> httpRq.anyRequest().permitAll())
-                .sessionManagement((session) ->
-                        session
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterAt(myCorsFilter, CorsFilter.class)
-                .build();
-
+    public CasAuthenticationProvider casAuthenticationProvider(IsepUserDetailsService userDetailsService) {
+        CasAuthenticationProvider provider = new CasAuthenticationProvider();
+        provider.setAuthenticationUserDetailsService(new UserDetailsByNameServiceWrapper<>(userDetailsService));
+        provider.setServiceProperties(serviceProperties());
+        provider.setTicketValidator(cas30ServiceTicketValidator());
+        provider.setKey("key");
+        return provider;
     }
 
     @Bean
-    @Primary
-    public CorsConfigurationSource config() {
-        return new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                System.out.println("Fetched cors config=");
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4200/"));
-                return config;
-            }
-        };
+    public TicketValidator cas30ServiceTicketValidator() {
+        return new Cas30ServiceTicketValidator("https://portail-ovh.isep.fr/cas");
     }
 
     @Bean
-    public WebMvcConfigurer corsconfig(){
-        return new WebMvcConfigurer() {
-            @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200", "http://localhost:4200/");
-            }};
+    public ServiceProperties serviceProperties() {
+        ServiceProperties serviceProperties = new ServiceProperties();
+        serviceProperties.setService("https://api.icompetences.tardieu.info/login/cas");
+        serviceProperties.setSendRenew(false);
+        return serviceProperties;
     }
 
-    CorsConfigurationSource apiConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4200/"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Bean
+    public CasAuthenticationEntryPoint casAuthenticationEntryPoint() {
+        CasAuthenticationEntryPoint entryPoint = new CasAuthenticationEntryPoint();
+        entryPoint.setLoginUrl("https://portail-ovh.isep.fr/cas/login");
+        entryPoint.setServiceProperties(serviceProperties());
+        return entryPoint;
     }
 
+    @Bean
+    public CasAuthenticationFilter casAuthenticationFilter(IsepUserDetailsService isepUserDetailsService) throws Exception {
+        CasAuthenticationFilter filter = new CasAuthenticationFilter();
+        filter.setAuthenticationManager(authentication -> casAuthenticationProvider(isepUserDetailsService).authenticate(authentication));
+        filter.setServiceProperties(serviceProperties());
+        return filter;
+    }
 
+    @Bean
+    public SingleSignOutFilter singleSignOutFilter() {
+        return new SingleSignOutFilter();
+    }
+
+    @Bean
+    public LogoutFilter logoutFilter() {
+        LogoutFilter logoutFilter = new LogoutFilter("https://portail-ovh.isep.fr/cas/logout", new SecurityContextLogoutHandler());
+        return logoutFilter;
+    }
 }
