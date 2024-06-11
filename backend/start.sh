b7b62828dd664d867ccc7cae50618d534bb926ea
Matthieu <matthieu.delarue5@gmail.com>
2024-06-11 15:03:38 +0200

    update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -2,6 +2,4 @@ db_password=LWpxst8USVhsc8VCWSQtUp7eqYgJQst3
 db_string=jdbc:postgresql://tardieu.info:54322/icompetences
 db_username=maxime
 jwt_secret=aHVnu3sQax9EGPetRDFHgdBoAn4dN9Kni5uvYfQn2cUKLZefrkkQxQsbbgmN3sCCRYB9zCGJfbpbuPnH2RHtuPAM4kGDtaZn8LkQrfUnrJYvWmgyjHPJLHPGbh7ihE3Z
-echo $jwt_secret
-./mvnw spring-boot:run 
-
+chmod +x ./mvnw  && ./mvnw spring-boot:run 
